07f883068a337184ce4edbed5e6161b1ecdd01c1
Nik Floyd <dragons.rise@gmail.com>
2023-08-24 00:15:04 -0500

    adding to lily58 rules.mk to ease compile

--- keyboards/splitkb/aurora/lily58/keymaps/debug/rules.mk
+++ keyboards/splitkb/aurora/lily58/keymaps/debug/rules.mk
@@ -15,9 +15,11 @@
 
 # NOTE: These are already enabled by default at the revision level
 #ENCODER_ENABLE = yes
-#OLED_ENABLE = yes
+OLED_ENABLE = no
 
 # RGB Matrix is enabled at the revision level,
 # while we use the regular RGB underglow for testing
 RGB_MATRIX_ENABLE = no
 RGBLIGHT_ENABLE = yes
+# Convert from Elite-C to Liatris
+CONVERT_TO=liatris
