2d6cca796eec12f29afbfcc0108bf0376a50c096
Nik Floyd <dragons.rise@gmail.com>
2023-09-19 00:17:58 -0500

    Adding lily58 files, but not functional with current forked repo

--- keyboards/splitkb/aurora/lily58/keymaps/debug/rules.mk
+++ keyboards/splitkb/aurora/lily58/keymaps/debug/rules.mk
@@ -14,16 +14,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # NOTE: These are already enabled by default at the revision level
-ENCODER_ENABLE = yes
-OLED_ENABLE = no
+#ENCODER_ENABLE = yes
+#OLED_ENABLE = yes
 
 # RGB Matrix is enabled at the revision level,
 # while we use the regular RGB underglow for testing
 RGB_MATRIX_ENABLE = no
 RGBLIGHT_ENABLE = yes
-
-# Convert from Elite-C to Liatris
-CONVERT_TO=liatris
-
-# split keyboard enable
-SPLIT_KEYBOARD = yes
